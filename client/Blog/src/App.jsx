b25cc38ccd50f2bf39fc94f92fe186b1f4b488c9
Ksohlo <alabssouls@gmail.com>
2024-01-24 09:03:03 +0100

    creating pages

--- client/Blog/src/App.jsx
+++ client/Blog/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import About from "./pages/About"
+import { BrowserRouter, Routes, Route} from react-router-dom
+import Home from "./pages/Home"
+import Project from "./pages/Project"
+import SignUp from "./pages/SignUp"
+import Signin from "./pages/signin"
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={Home} path={'/'} />
+        <Route element={About} path={'/about'} />
+        <Route element={Project} path={'/project'} />
+        <Route element={SignUp} path={'/signUp'} />
+        <Route element={Signin} path={'/signin'} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
